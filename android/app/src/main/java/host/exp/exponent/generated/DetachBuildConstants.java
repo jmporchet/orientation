097302c366e94a9a53b1710e3901602851e5a715
Jean-Marie <jm@jmporchet.ch>
2019-10-10 10:13:32 +0200

    chore: android build files

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expa9b7cf969e3541a8bdd39f567c5b96fc://10.70.1.4:19000";
 
 }
